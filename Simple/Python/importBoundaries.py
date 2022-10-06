Fixed rotation bug

Simple/Python/importBoundaries.py
@@ -9,7 +9,7 @@ def rotateNode(angle, node):
     length = math.sqrt(node[0] ** 2 + node[1] ** 2)
     theta = math.atan2(node[1], node[0])
     
-    theta = theta + math.radians(-angle)
+    theta = theta + math.radians(angle)
     
     x = length * math.cos(theta)
     y = length * math.sin(theta)
@@ -74,8 +74,6 @@ def hollowHalfCylinder(objs, My, Mx, xc, yc, r1, r2):
 
 
 def airFoil(fileName, length, angle, resolution):
-    pp = pprint.PrettyPrinter(indent=4)
-
     nodes = []
     rotatedNodes = []
 
@@ -96,8 +94,14 @@ def airFoil(fileName, length, angle, resolution):
     ys2 = ys[len(ys)//2:]
 
     for x in range(0, length + 1, resolution):
-        yVal1 = int(round(abs(np.interp(x, xs1, ys1))))
-        yVal2 = int(round(abs(np.interp(x, xs2, ys2))))
+        yVal1 = np.interp(x, xs1, ys1)
+        yVal1 = int(round(abs(yVal1))*np.sign([yVal1]))
+        yVal2 = np.interp(x, xs2, ys2)
+        yVal2 = int(round(abs(yVal2))*np.sign([yVal2]))
         boundaryNodes.append([x, yVal1, yVal2])
+
+    with open("out.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(boundaryNodes)
 
     return boundaryNodes

Simple/Python/simple.py
@@ -16,7 +16,7 @@ def importFoil(objs, fileName, Mx, P, wingLength, latticResolution, angle):
 	nodes = airFoil(fileName, wingLength, angle, latticResolution)
 
 	for point in nodes:
-		for y in range(point[1] + P[1], point[2] + P[1]):
+		for y in range(point[2] + P[1], point[1] + P[1]):
 			objs[y][point[0]+P[1]] = True
 
 	return objs
@@ -34,10 +34,11 @@ def main():
 
 	Nx = 800 # Number of nodes in the x direction
 	Ny = 200 # Number of nodes in the y direction
-	tau = 0.53 # Time constant for collision equation
-	Nt = 100 # Number of time steps
+	tau = 1 # Time constant for collision equation
+	Nt = 1000 # Number of time steps
 	Nl = 9 # Number of nodes around a lattice node
 	angle = 5
+	velocity = 6
 
 	foilNumber = 'naca0012'
 	foilPath = os.path.join(currentPath, r'../../Foils/' + foilNumber + '.csv')
@@ -48,7 +49,7 @@ def main():
 
 	F = np.ones((Ny, Nx, Nl)) + 0.01 * np.random.randn(Ny, Nx, Nl)
 
-	F[:, :, 3] = 2 # velocity in terms of relative speed of sound
+	F[:, :, 3] = velocity # velocity in terms of relative speed of sound
 
 	# setup object in lattice
 	obj = setupObjects(Ny, Nx, foilPath, angle)
@@ -121,7 +122,7 @@ def main():
 	print("\nSaving results...")
 	ani = animation.ArtistAnimation(f, ims, interval=50, blit=True, repeat_delay=1000)
 
-	saveName = "Results/" + foilNumber + "_" + str(angle) + "_" + str(Nx) + "x" + str(Ny) + "_" + str(Nt) + "s" + ".mp4"
+	saveName = "Results/" + foilNumber + "_" + str(velocity) + "_" + str(angle) + "_" + str(Nx) + "x" + str(Ny) + "_" + str(Nt) + "s" + ".mp4"
 
 	writer = animation.FFMpegWriter(
 		fps=15, metadata=dict(artist='Me'), bitrate=1800)